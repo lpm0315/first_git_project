3c9e7d32c07a10cf871febc8fa7a9ef8bb8e548c
liupanming <154598259@qq.com>
2019-04-09 15:57:41 +0800

    11

--- src/admin/test.java
+++ src/admin/test.java
@@ -5,7 +5,15 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to github!");
+		niubihonghong ta = new niubihonghong();
+		ta.setUser("xiaoming");
+		ta.setPassword("123456");
+		ta.setDiscript("…µµı“ª∫≈");
+		System.out.println(ta.getUser());
+		System.out.println(ta.getPassword());
+		System.out.println(ta.getDiscript());
 
 	}
 
 }
+	
