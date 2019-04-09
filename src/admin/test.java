7be887952597161c2377c538d889c1dc0d03346d
liupanming <154598259@qq.com>
2019-04-09 16:15:32 +0800

    22

--- src/admin/test.java
+++ src/admin/test.java
@@ -5,6 +5,9 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to github!");
+		shaziyo a=new shaziyo();
+		a.setId("1");
+		a.setCb("18s");
 
 	}
 
